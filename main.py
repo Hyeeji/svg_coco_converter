5e2cf623896d81c956c4888d9ef013263c08c386
Han Hyeji <77146030+Hyeeji@users.noreply.github.com>
2021-10-10 00:57:20 +0900

    feat : convert .SVG to .PNG

---
+++ main.py
@@ -0,0 +1,9 @@
+from cairosvg import svg2png
+#import cv2
+
+svg2png(url="file:///Users/hanhyeji/Documents/CVLab/bottom/bottom_shorts/slim_midi/0.svg", write_to="test.png")
+
+#image = cv2.imread('test.png')
+#cv2.imwrite('test.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+
