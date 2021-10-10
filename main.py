53f2fbba51656378f5b1650b908fbdf7c6fd3449
hyeeji <gpwl7597@jbnu.ac.kr>
2021-10-10 15:48:40 +0900

    fix : change library

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from cairosvg import svg2png
-#import cv2
 
-svg2png(url="file:///Users/hanhyeji/Documents/CVLab/bottom/bottom_shorts/slim_midi/0.svg", write_to="test.png")
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
 
-#image = cv2.imread('test.png')
-#cv2.imwrite('test.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+class svgToimage() :
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def svgConvert(self):
+        svg = svg2rlg(path=self.path)
+        renderPM.drawToFile(svg, 'test.jpg', fmt='JPG')
 
 
+if __name__ == '__main__':
+   path = input()
+   converter = svgToimage(path=path)
