ea93936e86cd45b77e48b38a92b4fae52f66879f
Hyungki Kim <diskhkme@gmail.com>
2021-11-11 15:54:10 +0900

    add comments

--- segmented_processing.py
+++ segmented_processing.py
@@ -8,6 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def convert_svg_to_png(svg_path):
+    # Convert each sub-SVG into image
+
     out_img_filepath = svg_path.replace('.svg', '.png')
     print("Writing {0}...".format(out_img_filepath))
     cairosvg.svg2png(url=segmented_file_path, write_to=segmented_file_path.replace('.svg', '.png'))
@@ -21,6 +23,7 @@ def convert_svg_to_png(svg_path):
 def process_img(img_path, width_stride):
     img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED) # Alpha channel read
 
+    # Since all meaningful information is colored in black (or whatever), we only care about the pixels which is not transparent
     bw_img = (img[:,:,3] > 0) # alhpa != 0
 
     width = bw_img.shape[1]
@@ -32,23 +35,26 @@ def process_img(img_path, width_stride):
     current_w = 0
     while current_w < width:
         column_arr = bw_img[:,current_w]
-        positive_indices = np.where(column_arr)
-        if len(positive_indices[0]) == 0:
+        positive_indices = np.where(column_arr) # for each column of pixels
+        if len(positive_indices[0]) == 0: # if there is no meaningful pixel, continue
             current_w += width_stride
             continue
 
+        # if there is meaningful pixel, gather topmost and bottommost pixel
         tops.append((current_w, np.max(positive_indices)))
         bottoms.append((current_w, np.min(positive_indices)))
 
+        # for every width_stride column
         current_w += width_stride
 
+    # Connect topmost and bottommost pixels and close the loop(=polygon)
     point_num = len(bottoms)
     for i in range(point_num):
         tops.append(bottoms[point_num-i-1])
     tops.append(tops[0]) # force closing
 
 
-    # Debug plot
+    # -- Plotting for debug
     x_vals = []
     y_vals = []
 
@@ -71,14 +77,19 @@ if __name__ == '__main__':
     segmented_root_path = './segmented_files'
     WIDTH_STRIDE = 1
 
+    # for each folder that contains sub-SVG,
     for segmented_svg_folder in os.listdir(segmented_root_path):
-
         svg_segment_folder = os.path.join(segmented_root_path,segmented_svg_folder)
+
+        # for each sub-SVG,
         for segmented_file in os.listdir(svg_segment_folder):
             if os.path.splitext(segmented_file)[1].find(".svg") is not -1:
                 segmented_file_path = os.path.join(svg_segment_folder,segmented_file)
+
+                # 1) convert svg to image
                 out_img_file = convert_svg_to_png(segmented_file_path)
 
+                # 2) process image and generate polygon
                 process_img(out_img_file, WIDTH_STRIDE)
 
 
--- svg_segmentation.py
+++ svg_segmentation.py
@@ -3,16 +3,23 @@ import copy
 import os
 
 def gather_name(node):
+    # Recursively gather name(=id)s for each base/sticker
+
     if 'path' in node.tag or 'line' in node.tag or 'polyline' in node.tag or 'polygon' in node.tag:
+        # Does not care about geometry tag name
         return ""
     else:
         for subnode in node:
             if "id" in node.attrib:
+                # Concatenate current name and call gather_name on child
                 return node.attrib["id"] + "-" + gather_name(subnode)
             else:
+                # If current tag does not have a name, just call on child
                 return "" + gather_name(subnode)
 
 def change_fill_style(node):
+    # Recursively change fill style as a black color, so that meaningful (internal) area is all presented in black
+
     if 'style' in node.attrib:
         start_idx = node.attrib['style'].find('fill:')
         if start_idx is not -1:
@@ -29,26 +36,33 @@ if __name__ == '__main__':
 
     svg_file = os.path.join(input_path,file_name + extention)
 
-    ET.register_namespace('', "http://www.w3.org/2000/svg")
+    ET.register_namespace('', "http://www.w3.org/2000/svg") # should add namespace
     tree = ET.parse(svg_file)
     root = tree.getroot()
     change_fill_style(root)
 
     segment_names = []
+    # First gather names for each base/sticker
     for first_lvl in root:
         segment_names.append(gather_name(first_lvl))
 
+    # Write each separated SVG file(sub-SVG) for base/sticker from source SVG
     for i in range(len(root)):
         tree_cpy = copy.deepcopy(tree)
         export_root = tree_cpy.getroot()
+        # Remove all children
         for first_lvl in list(export_root):
             export_root.remove(first_lvl)
 
+        # Attach only one level 1 node
         export_root.append(root[i])
 
+        # Make folder for source svg file and write base/sticker sub-SVG file
         out_folder = os.path.join(output_path, file_name)
         if not os.path.exists(out_folder):
             os.mkdir(out_folder)
+
+        # Filename contains layer 1 & 2 & 3 information
         tree_cpy.write("{0}/{1}_{2}{3}".format(out_folder, file_name, segment_names[i], extention), encoding="ASCII", xml_declaration=True)
 
 
