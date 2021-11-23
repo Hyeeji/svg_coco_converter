3bbd27a5262e4a8518a1c02480ed0efa56fe8b78
hyeeji <gpwl7597@jbnu.ac.kr>
2021-11-23 18:01:33 +0900

    Test

--- to_cocoformat.py
+++ to_cocoformat.py
@@ -1,1 +1,15 @@
 import json
+
+def write_coco_annotaion(annotation_data):
+
+    data = {}
+
+    data['annotations'] = []
+
+    for annotation in annotation_data:
+        data['annotations'].append({'id': annotation[0],
+                                    'Group': annotation[1],
+                                    'segementation': []})
+
+
+
